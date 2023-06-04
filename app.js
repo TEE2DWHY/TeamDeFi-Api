e663181d1f07918b82cd71b72e5c36bd4363fa22
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2023-06-04 18:45:42 +0100

    added extra security layers

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 const connectDb = require("./db/connect");
 const authRouter = require("./routers/auth");
 const errorHandler = require("./middleware/errorHandler");
@@ -9,12 +12,20 @@ const notFound = require("./middleware/notFound");
 
 //middleWares
 app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  })
+);
 app.use(cors());
-app.use("/api/v1", authRouter);
+app.use("/api/v1/auth", authRouter);
 app.use(notFound);
 app.use(errorHandler);
 
-const port = process.env.PORT || 6000;
+const port = 8000 || process.env.PORT;
 
 const start = async () => {
   try {
