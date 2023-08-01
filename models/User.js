46bf8554ffdbc8e98390037b522d0f1e6b63ad88
TEE2DWHY <isaacolorunfemi330@gmail.com>
2023-08-01 14:32:40 -0700

    made changes to user model

--- models/User.js
+++ models/User.js
@@ -24,8 +24,14 @@ const userSchema = new mongoose.Schema({
     required: [true, "Please provide password"],
     minLength: [8, "Password length should be minimum of 8 characters"],
   },
-  resetPasswordToken: { type: String, default: null },
-  resetPasswordExpires: { type: Date, default: null },
+  resetPasswordToken: {
+    type: String,
+    default: null,
+  },
+  resetPasswordExpires: {
+    type: Date,
+    default: null,
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
